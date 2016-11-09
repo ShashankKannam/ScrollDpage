b460dd9a875c7041f7eeea7c55783354c51b24c8
shashank kannam <shashank@Shashanks-MacBook-Pro.local>
2016-11-09 16:24:58 -0600

    Horizontally scrollable

--- ScrollDPage/ViewController.swift
+++ ScrollDPage/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+            }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+       //var distanceContent:CGFloat=0.0
+        var distance:CGFloat = 0.0
+        scrollView.backgroundColor=UIColor.purple.withAlphaComponent(0.7)
+        for x in 0...2
+            
+        {
+            let image = UIImage(named: "icon\(x).png")
+            
+            images.append(UIImageView(image: image))
+            
+            scrollView.addSubview(images[x])
+            
+            
+            distance = scrollView.frame.size.width/2 + scrollView.frame.size.width * CGFloat(x)
+            
+            //distanceContent = distance
+            
+            images[x].frame = CGRect(x: distance-75, y: scrollView.frame.size.height/2-75, width: 150, height: 150)
+        }
+        
+        scrollView.contentSize=CGSize(width:distance+scrollView.frame.size.width/2, height: scrollView.frame.size.height)
 
+    }
+    
+    
 }
 
